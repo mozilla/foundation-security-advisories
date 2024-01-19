4a9ff7397e0b6ef535722087abd29ddaf7ec9e07
Tom Ritter <tom@ritter.vg>
2024-01-19 12:17:07 -0500

    Add a way to assign the bugzilla alias even when you've already assigned the CVEs

--- foundation_security_advisories/assign_cve_ids.py
+++ foundation_security_advisories/assign_cve_ids.py
@@ -27,6 +27,17 @@ def main():
             print(f"\n-> {cve_id}")
             if replace_cve_id(cve_advisory):
                 try_set_bugzilla_alias(cve_id.split("-")[-1], cve_advisory.id)
+        elif cve_advisory.newest_instance.mfsa_id == os.getenv("ASSIGN_BUGZILLA_ANYWAYS", ""):
+            # In the above, we have the bug id from the placeholder. Here we need to get it from the references
+            if len(cve_advisory.newest_instance.references) == 1 and len(cve_advisory.newest_instance.references[0]) == 2 and \
+                cve_advisory.newest_instance.references[0][1] is None and\
+                cve_advisory.newest_instance.references[0][0].startswith("https://bugzilla.mozilla.org/show_bug.cgi?id="):
+                bug_id = cve_advisory.newest_instance.references[0][0].replace("https://bugzilla.mozilla.org/show_bug.cgi?id=", "")
+                try_set_bugzilla_alias(bug_id, cve_advisory.id)
+            elif cve_advisory.newest_instance.title.startswith("Memory safety bugs fixed")
+                pass # Roll-up
+            else:
+                print(f"{cve_advisory.newest_instance.parent.id} didn't have an expected reference format so you have to set it manually.")
 
     if os.getenv("CI"):
         subprocess.run(
